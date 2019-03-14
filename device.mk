f10b37db570d00e7c59e59a4e1ee89bd483a6a7d
DarknessKiller <kaelvinchen2@gmail.com>
2019-03-14 15:37:23 +0800

    Add Prebuilt Google Camera
    * MGC_3.2.045_RazerPhone2_V1.apk

--- device.mk
+++ device.mk
@@ -152,7 +152,7 @@ PRODUCT_PACKAGES += \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     camera.device@3.3-impl \
-    Snapcam
+    GoogleCamera
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
