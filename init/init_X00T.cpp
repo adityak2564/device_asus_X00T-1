8e14e7ea293b7fc8d5ddbb5b72a5a6122ba06892
Rizky Benggolo <travarilo@gmail.com>
2019-03-17 02:52:54 +0800

    X00T: one product name for all variants

--- init/init_X00T.cpp
+++ init/init_X00T.cpp
@@ -59,12 +59,6 @@ void property_override(char const prop[], char const value[])
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
-void property_override_dual(char const system_prop[], char const vendor_prop[], char const value[])
-{
-    property_override(system_prop, value);
-    property_override(vendor_prop, value);
-}
-
 void property_override_triple(char const system_prop[], char const vendor_prop[], char const bootimg_prop[], char const value[])
 {
     property_override(system_prop, value);
@@ -85,23 +79,15 @@ void vendor_check_variant()
     if (ReadFileToString(region_file, &region))
         region = Trim(region);
 
-    // Russian model has a slightly different product name
-    if (region == "RU")
-        product_name = "RU_X00TD";
-    else
-        product_name = "WW_X00TD";
-
     // 6 GB variant
     if (sys.totalram > 4096ull * 1024 * 1024) {
         // Russian model
         if (region == "RU") {
             build_fingerprint = "asus/RU_X00TD/ASUS_X00T_9:9/PKQ1/16.2017.1905.053-20190513:user/release-keys";
-            product_device = "ASUS_X00T_9";
 
         // Global model
         } else {
             build_fingerprint = "asus/WW_X00TD/ASUS_X00T_3:9/PKQ1/16.2017.1905.053-20190513:user/release-keys";
-            product_device = "ASUS_X00T_3";
         }
 
     // 3/4 GB variants
@@ -109,26 +95,14 @@ void vendor_check_variant()
         // Russian model
         if (region == "RU") {
             build_fingerprint = "asus/RU_X00TD/ASUS_X00T_6:9/PKQ1/16.2017.1905.053-20190513:user/release-keys";
-            product_device = "ASUS_X00T_6";
 
         // Global model
         } else {
             build_fingerprint = "asus/WW_X00TD/ASUS_X00T_2:9/PKQ1/16.2017.1905.053-20190513:user/release-keys";
-            product_device = "ASUS_X00T_2";
         }
     }
 
-    // Product model overrides
-    if (region == "RU" || region == "TW" ||
-        (region == "PH" && sys.totalram > 3072ull * 1024 * 1024))
-        product_model = "ZB602KL";
-    else
-        product_model = "ZB601KL";
-
     // Override props based on values set
-    property_override_dual("ro.product.device", "ro.vendor.product.device", product_device);
-    property_override_dual("ro.product.model", "ro.vendor.product.model", product_model);
-    property_override_dual("ro.product.name", "ro.vendor.product.name", product_name);
     property_override_triple("ro.build.fingerprint", "ro.vendor.build.fingerprint", "ro.bootimage.build.fingerprint", build_fingerprint);
 
     // Set region code via ro.config.versatility prop
