f2d3621dbe63de614a056ebb3beda28501593c0f
anomalchik <vidslava@yandex.ru>
2019-08-12 19:49:16 +0000

    X00T: update to S055/056

--- lineage_X00T.mk
+++ lineage_X00T.mk
@@ -37,7 +37,7 @@ TARGET_VENDOR := asus
 TARGET_VENDOR_PRODUCT_NAME := X00T
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="sdm660_64-user 9 PKQ1 1181 release-keys"
+    PRIVATE_BUILD_DESC="sdm660_64-user 9 PKQ1 1245 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Android/sdm660_64/sdm660_64:9/PKQ1/16.2017.1905.053-20190513:user/release-keys
+BUILD_FINGERPRINT := Android/sdm660_64/sdm660_64:9/PKQ1/16.2017.1906.055-20190703:user/release-keys
